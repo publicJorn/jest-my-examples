f5f7193835cb1534d60b9c68eb23261672568558
Jorn Luiten <js.luiten@gmail.com>
2017-04-22 21:44:59 +0200

    Explained why we need to mock factory functions

--- __tests__/jest-mock.test.js
+++ __tests__/jest-mock.test.js
@@ -1,21 +1,26 @@
+// 1.
 // The test passes while not using a mock file, but this takes a lot of system resources (see console)
 // More importantly, it's not a unit test. If something is wrong in `myLib` the test for `code` will fail!
 // That's why we mock it.
 // Note, that since we use the answer property on `myLib` we need to provide it in our mock implementation
 // jest.mock('../myLib', () => ({ answer: jest.fn() }))
 
+// 2.
 // If you uncommented the above line, you'll see that the first test fails.
 // That's because `code` depends on `myLib.answer` to return a value of 42.
 // Since we mocked it without returning a value, it won't return the expected value of 43.
 // jest.mock('../myLib', () => ({ answer: () => 42 })) // or `answer: () => jest.fn(() => 42)`
 
+// 3.
 // If a manual mock file is present (guess what..) we don't need to write an implementation here;
 // The manual mock will be used. We still need it to return 42 though, or the first test will fail.
-jest.mock('../myLib')
+// jest.mock('../myLib')
 
-// We also mock the loggerFactory, because we don't want our console to be spammed with logs during a test run.
-// It has a manual mock file, so it automatically works
-// jest.mock('../loggerFactory')
+// 4.
+// We _need_ to mock the loggerFactory, because it's a factory...
+// Otherwise both the code and the test create a separate instance of the logger and the spy won't capture the call!
+// We use a manual mock file, because loggerFactory is likely to be used in other tests as well.
+jest.mock('../loggerFactory')
 
 const myLib = require('../myLib')
 const logger = require('../loggerFactory')()
